27c753b5f9afe32717227a4f65cf8ffedd18c914
jamescc2000 <jamescontrerasc458@gmail.com>
2021-11-06 02:06:41 -0500

    Registro de Servicios

--- app/src/main/java/com/example/talma/RegistrarServiciosFragment.java
+++ app/src/main/java/com/example/talma/RegistrarServiciosFragment.java
@@ -1,5 +1,6 @@
 package com.example.talma;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TableLayout;
+
+import java.util.ArrayList;
 
 
 public class RegistrarServiciosFragment extends Fragment {
 
-
+    private TableLayout table_servicios;
+    private String [] header = {"Nombre", "Codigo", " "};
+    private ArrayList<String[]> rows = new ArrayList<>();
+    private TablaDinamica tablaDinamica;
 
     public RegistrarServiciosFragment() {
         // Required empty public constructor
@@ -30,6 +38,28 @@ public class RegistrarServiciosFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_registrar_servicios, container, false);
 
+        table_servicios = (TableLayout) view.findViewById(R.id.table_servicios);
+
+        tablaDinamica = new TablaDinamica(table_servicios, view.getContext());
+        tablaDinamica.addHeader(header);
+        tablaDinamica.addData(getServicios());
+        tablaDinamica.backgroundHeader(Color.BLACK);
+        tablaDinamica.backgroundData(Color.GRAY, Color.DKGRAY);
+        tablaDinamica.lineColor(Color.BLACK);
+        tablaDinamica.textColorData(Color.BLACK);
+        tablaDinamica.textColorHeader(Color.WHITE);
+
         return view;
     }
+
+    public void agregar(View view){
+        String [] item = new String[] {"Remolque", "SRMQ167"};
+        tablaDinamica.addItem(item);
+    }
+
+    private ArrayList <String[]> getServicios(){
+        rows.add(new String[]{"Escala tecnica", "SET124"});
+        return rows;
+    }
+
 }
