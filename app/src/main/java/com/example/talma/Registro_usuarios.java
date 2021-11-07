eb93e623c3c1cc53258a930053f98b003de698ae
Richard1471 <richard.huallpa.l@uni.pe>
2021-11-06 22:45:01 -0500

    Registro de usuarios

---
+++ app/src/main/java/com/example/talma/Registro_usuarios.java
@@ -0,0 +1,14 @@
+package com.example.talma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro_usuarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_usuarios);
+    }
+}
