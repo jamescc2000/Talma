40908ca68a7d038e57063c77c93828e90f2082af
Richard1471 <richard.huallpa.l@uni.pe>
2021-11-06 20:29:22 -0500

    Atención de reclamos

---
+++ app/src/main/java/com/example/talma/Atencion_reclamos.java
@@ -0,0 +1,14 @@
+package com.example.talma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Atencion_reclamos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atencion_reclamos);
+    }
+}
