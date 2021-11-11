f8da0eda1a104d25938907b8c2260519acf09096
Richard1471 <richard.huallpa.l@uni.pe>
2021-11-11 15:10:30 -0500

    Facturación

---
+++ app/src/main/java/com/example/talma/Facturacion.java
@@ -0,0 +1,13 @@
+package com.example.talma;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Facturacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facturacion);
+    }
+}
