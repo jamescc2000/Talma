cf69818bfe9c3a778993e31dbd0d0f86055c3d83
jamescc2000 <jamescontrerasc458@gmail.com>
2021-11-16 20:33:35 -0500

    Coneccion con Firebase

--- app/src/main/java/com/example/talma/RsirEmpleados/Facturacion.java
+++ app/src/main/java/com/example/talma/RsirEmpleados/Facturacion.java
@@ -1,16 +1,33 @@
 package com.example.talma.RsirEmpleados;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.talma.Nueva_factura;
 import com.example.talma.R;
 
 public class Facturacion extends AppCompatActivity {
+
+    Button btn_nueva_factura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facturacion);
+
+        btn_nueva_factura = (Button) findViewById(R.id.btn_nueva_factura);
+
+        btn_nueva_factura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Facturacion.this, Nueva_factura.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
