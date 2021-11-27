a47f1311e9cf6770769b82ba7c304eef84809319
Richard1471 <richard.huallpa.l@uni.pe>
2021-11-26 20:27:34 -0500

    fragment facturacion

--- app/src/main/java/com/example/talma/Fragmentos_empleados/FacturasFragment.java
+++ app/src/main/java/com/example/talma/Fragmentos_empleados/FacturasFragment.java
@@ -1,5 +1,6 @@
 package com.example.talma.Fragmentos_empleados;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.talma.Facturacion;
+import com.example.talma.Nueva_factura;
 import com.example.talma.R;
+
 
 
 public class FacturasFragment extends Fragment {
 
+    Button btn_nueva_factura, btn_visualizar_factura;
+
     public FacturasFragment() {
         // Required empty public constructor
     }
+
+
 
 
 
@@ -31,6 +40,15 @@ public class FacturasFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_facturas, container, false);
 
+        btn_nueva_factura = (Button) view.findViewById(R.id.btn_nueva_factura);
+
+        btn_nueva_factura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(view.getContext(), Nueva_factura.class);
+                startActivity(intent);
+            }
+        });
 
 
         return view;
