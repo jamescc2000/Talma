1218d709acd31675057229f04d6970404e6334cb
Richard1471 <richard.huallpa.l@uni.pe>
2021-12-02 16:31:49 -0500

    hgfhd

--- app/src/main/java/com/example/talma/Modelos/ModeloFactura.java
+++ app/src/main/java/com/example/talma/Modelos/ModeloFactura.java
@@ -2,14 +2,27 @@ package com.example.talma.Modelos;
 
 
 public class ModeloFactura {
-    private float dias;
+    private long dias;
     private float peso;
 
-
-    public ModeloFactura() {
-
+    public ModeloFactura(long dias, float peso) {
+        this.dias = dias;
+        this.peso = peso;
     }
 
+    public long getDias() {
+        return dias;
+    }
 
+    public void setDias(int dias) {
+        this.dias = dias;
+    }
 
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
 }
--- app/src/main/java/com/example/talma/Nueva_factura.java
+++ app/src/main/java/com/example/talma/Nueva_factura.java
@@ -8,12 +8,16 @@ import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.talma.Adapters.AdaptadorListaServicios;
+import com.example.talma.Modelos.ModeloFactura;
+import com.example.talma.Modelos.ModeloServicio;
 import com.example.talma.RsirEmpleados.RegistrarRsire;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -25,22 +29,36 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class Nueva_factura extends AppCompatActivity {
 
     private EditText et_RSIR,et_encargado_facturacion;
     private Button btn_fecha_emision;
 
+    //AdaptadorListaDatosFactura adaptadorListaDatosFactura;
+
+    List<ModeloFactura> ListaDatosFactura = new ArrayList <>();
+
     FirebaseAuth firebaseAuth;
     FirebaseUser user;
-    DatabaseReference bd_servicios;
+    DatabaseReference bd_servicio;
     DatePickerDialog datePickerDialog;
     ProgressDialog progressDialog;
+
+    String codigo_rsir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +73,9 @@ public class Nueva_factura extends AppCompatActivity {
         user = FirebaseAuth.getInstance().getCurrentUser();
         progressDialog = new ProgressDialog(Nueva_factura.this);
 
-        bd_servicios = FirebaseDatabase.getInstance().getReference("facturas");
+        bd_servicio = FirebaseDatabase.getInstance().getReference("servicios");
+        Intent intent = getIntent();
+        codigo_rsir = intent.getStringExtra("codigoRsir");
 
         btn_fecha_emision.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -111,7 +131,7 @@ public class Nueva_factura extends AppCompatActivity {
         datosNuevaFactura.put("EncargadoFacturacion", encargado_facturacion_String);
         datosNuevaFactura.put("FechaEmision", fecha_emision_String);
 
-        bd_servicios.child(uid_String).setValue(datosNuevaFactura).addOnCompleteListener(new OnCompleteListener<Void>() {
+        bd_servicio.child(uid_String).setValue(datosNuevaFactura).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
 
@@ -155,5 +175,66 @@ public class Nueva_factura extends AppCompatActivity {
             }
         })
     }*/
+
+    private void ObtenerServicios(){
+
+        Query servicioQuery = bd_servicio.orderByChild("codigoRSIR").equalTo(codigo_rsir);
+
+        servicioQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot ds: snapshot.getChildren()){
+
+                    //Obtenemos los datos
+                    Float  peso_float = ds.getValue(Float.class) ;
+                    String fecha_salida = ""+ ds.child("fecha_salida").getValue();
+                    String fecha_llegada = ""+ ds.child("fecha_llegada").getValue();
+
+                    try {
+
+                        SimpleDateFormat sdf_salida = new SimpleDateFormat("dd/mm/yyyy");
+                        SimpleDateFormat sdf_llegada = new SimpleDateFormat("dd/mm/yyyy");
+
+                        Date date_salida = sdf_salida.parse(fecha_salida);
+                        Date date_llegada = sdf_salida.parse(fecha_llegada);
+
+                        long dif = date_salida.getTime() - date_llegada.getTime();
+
+                        TimeUnit time = TimeUnit.DAYS;
+                        long diferencia =  time.convert(dif,TimeUnit.MILLISECONDS);
+
+                        //Colocamos los datos
+                        ListaDatosFactura.add(new ModeloFactura(diferencia,peso_float));
+
+                        //adaptadorListaDatosFactura = new AdaptadorListaDatos (Nueva_factura.this, ListaDatosFactura,"","");
+                        //adaptadorListaDatosFactura.notifyDataSetChanged();
+
+
+                    } catch (Exception e){
+
+                    }
+
+
+
+                    /*Colocamos los datos
+                    listaServicios.add(new ModeloServicio(user.getUid(), codigo_rsir,nombre_string, codigo_servicio_string, hora_desde_llegada_string, hora_hasta_llegada_string,
+                            hora_desde_salida_string, hora_hasta_salida_string, cantidad_llegada_string, cantidad_salida_string, estado_string));
+
+                    adaptadorListaServicios = new AdaptadorListaServicios(ValidarServicios.this, listaServicios, "revisar", "cliente");
+                    adaptadorListaServicios.notifyDataSetChanged();
+                    rv_lista_servicios.setAdapter(adaptadorListaServicios);*/
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
 
 }
